8ea388ee11234d88c9dd2465255138b40572f126
Chetan <cjgiridhar@example.com>
2014-12-15 10:39:09 +0530

    Rest API Example

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,18 @@
+package hello;
+
+/**
+ * Created by cgiridhar on 25/11/14.
+ */
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
